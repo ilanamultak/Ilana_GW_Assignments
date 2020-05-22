e843aea7564acc0fbc27cb6c39db52dba4bdd21a
Ilana <61954079+ijm5053@users.noreply.github.com>
2020-05-22 17:11:17 -0400

    Add files via upload

---
+++ Python-APIs/api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = '9140644abc2ac79b1091ff041f2b0ab3'
+
+# Google API Key
+g_key = 'AIzaSyCmirp-N_g5DXO9Icp6_Rk-ZwmB33FxdBg'
+
+api_key = 'AIzaSyCmirp-N_g5DXO9Icp6_Rk-ZwmB33FxdBg'
